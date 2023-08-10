f621ba6e1821580facf2a5899a62be91863737a6
GitHub Action <action@github.com>
2023-08-10 08:03:25 +0000

    Template cleanup

--- src/main/kotlin/com/github/notbetop/easyrun/services/MyProjectService.kt
+++ src/main/kotlin/com/github/notbetop/easyrun/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.notbetop.easyrun.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.notbetop.easyrun.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
--- src/main/kotlin/com/github/notbetop/easyrun/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/notbetop/easyrun/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.notbetop.easyrun.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.notbetop.easyrun.MyBundle
+import com.github.notbetop.easyrun.services.MyProjectService
 import javax.swing.JButton
 
 
